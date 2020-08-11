f9fa8c0d7880e5297f6bb14b8834b409f0a02555
mrrocketraccoon <ricardoxcm@hotmail.com>
2020-08-11 22:57:25 +0200

    Implementation of the 1-d Kalman-Filter algorithm.
    This implementation shows the most basic implementation of a Kalman filter
    from scratch in C++
    
    First implementation of algorithm

---
+++ cmake-build-debug/CMakeFiles/1dKalmanFilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1dKalmanFilter"
+  "1dKalmanFilter.pdb"
+  "CMakeFiles/1dKalmanFilter.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1dKalmanFilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
